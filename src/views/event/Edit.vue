1e327fc367c96d0724c80349ec3859f08391129e
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-07-29 14:25:23 +0700

    1.6

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,26 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="editbut">Edit data</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    editbut(){
+      this.GStore.flashMessage =
+        'Your data for' + this.event.title +'has been updated'
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
